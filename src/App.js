fd6598775d9db18731b4b47ad057333e836f3763
Briton Otieno <ojwangbriton@gmail.com>
2023-07-22 19:55:07 +0300

    Add base components

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import CategoryPage from './components/CategoryPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="nav-bar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/categories">Categories</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<CategoryPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+export default App;
