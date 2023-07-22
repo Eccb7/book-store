7be3fa33824b7beaef2921d4b62e6cd94cc38b3d
Briton Otieno <ojwangbriton@gmail.com>
2023-07-22 19:52:37 +0300

    Add BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IndividualBook from './IndividualBook';
+
+const BookList = ({ books, onDelete }) => (
+  <div>
+    <h2>Book List</h2>
+    {books.map((book) => (
+      <IndividualBook key={book.id} book={book} onDelete={onDelete} />
+    ))}
+  </div>
+);
+
+BookList.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default BookList;
